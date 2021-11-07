77c38c8aafd223e051daae0efae6ff5b7da1a872
rpaezbas <rafapaezbas@github.com>
2021-11-07 21:27:17 +0100

    added op modifier

--- assembler.js
+++ assembler.js
@@ -1,4 +1,4 @@
-const { sequenceOf, whitespace, endOfInput, anyOfString, choice, char, anyChar, letters, many, exactly, everyCharUntil } = require('arcsecond')
+const { sequenceOf, whitespace, endOfInput, anyOfString, str, choice, char, anyChar, possibly, letters, many, exactly, everyCharUntil } = require('arcsecond')
 const fs = require('fs')
 const path = require('path')
 const file = fs.readFileSync('./hello-world.tal').toString()
@@ -10,6 +10,12 @@ const toHex = (i) => {
   i = i.toString(16)
   return i.length < 2 ? '0' + i : i
 }
+
+const opCodes = ['BRK', 'INC', 'POP', 'DUP', 'NIP', 'SWP', 'OVR', 'ROT', 'EQU', 'NEQ', 'GTH', 'LTH', 'JMP', 'JCN', 'JSR', 'STH', 'LDZ', 'STZ', 'LDR', 'STR', 'LDA', 'STA', 'DEI', 'DEO', 'ADD', 'SUB', 'MUL', 'DIV', 'AND', 'ORA', 'EOR', 'SFT']
+const hexOpCodes = new Map()
+opCodes.forEach((op, i) => hexOpCodes.set(op, toHex(i)))
+hexOpCodes.set('LIT', '80')
+opCodes.push('LIT')
 
 const addIODevice = (e) => {
   let pad = e.value[0].value[1].join('')
@@ -25,10 +31,12 @@ const addIODevice = (e) => {
   })
 }
 
-const opCodes = ['BRK', 'INC', 'POP', 'DUP', 'NIP', 'SWP', 'OVR', 'ROT', 'EQU', 'NEQ', 'GTH', 'LTH', 'JMP', 'JCN', 'JSR', 'STH', 'LDZ', 'STZ', 'LDR', 'STR', 'LDA', 'STA', 'DEI', 'DEO', 'ADD', 'SUB', 'MUL', 'DIV', 'AND', 'ORA', 'EOR', 'SFT']
-const hexOpCodes = new Map()
-opCodes.forEach((op, i) => hexOpCodes.set(op, toHex(i)))
-hexOpCodes.set('LIT', '80')
+const op = (e) => {
+  const k = e.filter(e => e === 'k')[0] ? 0x80 : 0x00
+  const r = e.filter(e => e === 'r')[0] ? 0x40 : 0x00
+  const s = e.filter(e => e === '2')[0] ? 0x20 : 0x00
+  return (parseInt(hexOpCodes.get(e[0]), 16) + k + r + s).toString(16)
+}
 
 const hexadecimal = anyOfString('0123456789abdcef')
 const label = sequenceOf([char('@'), letters]).map(e => ({ type: 'label', value: e }))
@@ -45,9 +53,11 @@ const mainMemoryPad = sequenceOf([char('|'), exactly(4)(hexadecimal)]).map(e => 
 const comment = sequenceOf([char('('), everyCharUntil(char(')')), char(')')]).map(e => ({ type: 'comment', value: e }))
 const literalNumber = sequenceOf([hexadecimal, hexadecimal]).map(e => ({ type: 'literalNumber', value: e }))
 const push = sequenceOf([char('#'), hexadecimal, hexadecimal]).map(e => ({ type: 'push', value: e }))
+const opModifier = choice([char('k'), char('r'), char('2')])
+const ops = sequenceOf([choice(opCodes.map(e => str(e))), possibly(opModifier), possibly(opModifier), possibly(opModifier)]).map(e => ({ type: 'op', value: e }))
 const word = letters.map(e => ({ type: 'word', value: e }))
 
-const parser = sequenceOf([many(choice([comment, device, macro, sublabelAddress, pad, label, mainMemoryPad, ioPad, literalChar, literalNumber, push, word, whitespace])), endOfInput])
+const parser = sequenceOf([many(choice([comment, device, macro, sublabelAddress, pad, label, mainMemoryPad, ioPad, literalChar, literalNumber, push, ops, word, whitespace])), endOfInput])
 
 const assemble = (code) => {
   const context = parser.run(code)
@@ -82,8 +92,10 @@ const assemble = (code) => {
         return e.value[0] + e.value[1]
       case 'push':
         return '80' + e.value[1] + e.value[2]
+      case 'op':
+        return op(e.value)
       case 'word':
-        return hexOpCodes.get(e.value) !== undefined ? hexOpCodes.get(e.value) : macros.get(e.value)
+        return macros.get(e.value) // TODO throw if word doesnt exist
       default:
     }
     return ''
